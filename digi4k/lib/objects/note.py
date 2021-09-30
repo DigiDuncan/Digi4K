825d13340dad7960d84319366a05f819d1026435
DigiDuncan <digiduncan@gmail.com>
2021-09-30 01:57:19 -0400

    split Chart into Song and Chart

--- digi4k/lib/objects/note.py
+++ digi4k/lib/objects/note.py
@@ -10,6 +10,9 @@ class Flags:
     normal = "normal"
 
 
+arrowmap = ["⬅", "⬇", "⬆", "➡"]
+
+
 @total_ordering
 class ChartNote:
     def __init__(self, pos: float, lane: int, length: float):  # I think every time should be in ms and sure
@@ -20,6 +23,10 @@ class ChartNote:
 
         self.hit = False
         self.missed = False
+
+    @property
+    def pos_secs(self):
+        return self.pos / 1000
 
     def hittable(self, current_time: float, window: tuple[float, float]):
         offset = current_time - self.pos
@@ -38,10 +45,16 @@ class ChartNote:
         self.pos < other.pos
 
     def __repr__(self):
-        return f"<{self.pos}, {self.lane}, {self.length}, {self.flag}>"
+        return f"<Note {self.pos}, {self.lane}, {self.length}, {self.flag}>"
 
     def __str__(self):
-        return f"<{self.pos}, {self.lane}, {self.length}, {self.flag}>"
+        flag = "" if self.flag == "normal" else self.flag.title() + " "
+        arrow = arrowmap[self.lane]
+        pos = f" P {round(self.position, 4)}"
+        length = "" if self.length == 0 else f" L {round(self.length, 4)}"
+        hit = " HIT" if self.hit else ""
+        missed = " MISSED" if self.missed else ""
+        return f"<Note {flag}{arrow}{pos}{length}{hit}{missed}>"
 
 
 # This is for stuff in charts that's not notes
@@ -54,13 +67,19 @@ class ChartEvent:
 
 
 class Chart:
-    def __init__(self, name: str, diff: int, player: int, bpm: float, notespeed: float, notes: list[ChartNote], events: list[ChartEvent]):
-        self.name = name
-        self.diff = diff
+    def __init__(self,  player: int, notespeed: float, notes: list[ChartNote], events: list[ChartEvent]):
         self.player = player
-        self.bpm = bpm
         self.notespeed = notespeed
         self.notes = sorted(notes)
+        self.events = sorted(events)
+
+
+class Song:
+    def __init__(self, name: str, diff: int, bpm: float, charts: list[Chart], events: list[ChartEvent]):
+        self.name = name
+        self.diff = diff
+        self.bpm = bpm
+        self.charts = charts
         self.events = sorted(events)
 
     # Might be helpful?
@@ -74,15 +93,43 @@ class Chart:
         name = song["song"]
         bpm = song["bpm"]
         notespeed = song["speed"]
-        notes: list[ChartNote] = []
+        p1notes: list[ChartNote] = []
+        p2notes: list[ChartNote] = []
+        songevents: list[ChartEvent] = []
+        p1events: list[ChartEvent] = []
+        p2events: list[ChartEvent] = []
         sections = song["notes"]
+        lastMHS = None
         for section in sections:
-            sectionNotes = section["sectionNotes"]
+
+            # Create a camera focus event like they should have in the first place
+            mustHitSection: bool = section["mustHitSection"]
+            if mustHitSection != lastMHS:
+                if mustHitSection is True:  # fuck how do I get the current pos here
+                    songevents.append(ChartEvent(..., "camera_focus", focus = "player1"))
+                else:
+                    songevents.append(ChartEvent(..., "camera_focus", focus = "player2"))
+
+            # Actually make two charts
+            sectionNotes: list = section["sectionNotes"]
             if sectionNotes:
                 for note in sectionNotes:
-                    notes.append(ChartNote(*note))  # expansions ahoy
+                    low = (0, 1, 2, 3)  # wow look at the hardcoding~!
+                    high = (4, 5, 6, 7)
+                    pos, lane, length = note  # hope this never breaks lol
+                    if mustHitSection:
+                        if lane in low:
+                            p1notes.append(ChartNote(pos, lane, length))
+                        elif lane in high:
+                            p2notes.append(ChartNote(pos, lane - 4, length))
+                    else:
+                        if lane in high:
+                            p1notes.append(ChartNote(pos, lane - 4, length))
+                        elif lane in low:
+                            p2notes.append(ChartNote(pos, lane, length))
         # diff is hardcoded right now because I don't know how to extract it from
         # the chart. I think it's just based on the name.
-        # I'm also ignoring events for now since they don't technically exist in
-        # charts and I'm going to be generating them.
-        return Chart(name = name, player = 1, diff = 2, bpm = bpm, notespeed = notespeed, notes = notes, events = [])
+        return Song(name = name, diff = 2, bpm = bpm, charts = [
+            Chart(1, notespeed, p1notes, p1events),
+            Chart(2, notespeed, p2notes, p2events)
+        ], events = songevents)
