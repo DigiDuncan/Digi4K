d2ce0e220b8924878babaa07996e241b3553781d
DigiDuncan <digiduncan@gmail.com>
2021-09-30 02:23:49 -0400

    showfps

--- digi4k/main.py
+++ digi4k/main.py
@@ -5,7 +5,7 @@ from digi4k.lib.gamemanager import GameManager
 
 class Game(nygame.Game):
     def __init__(self):
-        super().__init__(size=(1280, 720), bgcolor=0xAAAAAA)
+        super().__init__(size=(1280, 720), bgcolor=0xAAAAAA, fps = 120, showfps = True)
         self.gm = GameManager(self)
 
     def loop(self, events):
