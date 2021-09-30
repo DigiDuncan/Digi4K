1edc9b73762170e48b98fb9232e64846e6c3c22d
DigiDuncan <digiduncan@gmail.com>
2021-09-30 01:56:36 -0400

    actually test highway

--- digi4k/lib/gamemanager.py
+++ digi4k/lib/gamemanager.py
@@ -1,48 +1,36 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+from digi4k.lib.objects.note import Chart
+
 if TYPE_CHECKING:
     from digi4k.main import Game
 
-from pygame import K_LEFT, K_RIGHT, K_LEFTBRACKET, K_RIGHTBRACKET
-
 from digi4k.lib.inputmanager import InputManager
-from digi4k.lib import ptext
 
-from digi4k.lib.draw_objects import DisplayNote, Note
+from digi4k.lib.draw_objects import Highway
+from importlib.resources import files
+import json
+
+import digi4k.data.tutorial
+
+from nygame import music
 
 
 class GameManager:
     def __init__(self, game: Game):
         self.game = game
-        self.lanes = [0, 1, 2, 3]
-        self.flags = ["normal", "bomb", "death", "gold", "unknown"]
-
-        self.current_lane: int = 0
-        self.current_flag: int = 0
+        self.chart_file = j = json.loads(files(digi4k.data.tutorial).joinpath("tutorial-hard.json").read_text())
+        self.chart = Chart.from_json(j)
+        self.highway = Highway(self.chart)
+        music.play(files(digi4k.data.tutorial).joinpath("Tutorial_Inst.ogg"))
 
         self.input = InputManager()
-        self.old_out = ""
 
     def update(self, events: list):
+        now = music.elapsed
         self.input.update(events)
-        # if self.input.has_changes:
-        #     print(self.input)
-
-        if K_LEFT in self.input.justPressed:
-            self.current_lane = (self.current_lane - 1) % self.lanes
-        elif K_RIGHT in self.input.justPressed:
-            self.current_lane = (self.current_lane + 1) % self.lanes
-        elif K_LEFTBRACKET in self.input.justPressed:
-            self.current_lane = (self.current_flag - 1) % self.flags
-        elif K_RIGHTBRACKET in self.input.justPressed:
-            self.current_lane = (self.current_flag + 1) % self.flags
-
-        lane = self.lanes[self.current_lane]
-        flag = self.flags[self.current_flag]
-
-        note = DisplayNote(Note(lane, 0, 0, flag = flag))
-        note_rect = note.sprite.get_rect()
-        note_rect.center = self.game.surface.get_rect().center
-        self.game.surface.blit(note.sprite, note_rect)
-        ptext.draw(f"{self.flags[self.current_flag]} {self.current_lane}", bottomleft=note_rect.move(0, -10).topleft, sysfontname="Lato Medium", fontsize=48, surf=self.game.surface)
+        self.highway.update(now)
+        highway_rect = self.highway._image.get_rect()
+        highway_rect.center = self.game.surface.get_rect().center
+        self.game.surface.blit(self.highway._image, (0, 0))
