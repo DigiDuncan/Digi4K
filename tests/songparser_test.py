6e11f617e3a02a7209e73e98f9f12b5b358427bd
DigiDuncan <digiduncan@gmail.com>
2021-10-01 14:39:09 -0400

    fix test

--- tests/songparser_test.py
+++ tests/songparser_test.py
@@ -1,11 +1,11 @@
 from importlib.resources import files
 import json
 
-import digi4k.data.tutorial
+import digi4k.data.charts.tutorial
 from digi4k.lib.objects.note import Chart
 
 
 def test_parse():
-    j = json.loads(files(digi4k.data.tutorial).joinpath("tutorial-hard.json").read_text())
+    j = json.loads(files(digi4k.data.charts.tutorial).joinpath("tutorial-hard.json").read_text())
     chart = Chart.from_json(j)
     assert chart is not None
