3268d17a923b9251a64e7fdcc809b9376a577fa3
DigiDuncan <digiduncan@gmail.com>
2021-10-01 04:58:27 -0400

    count hits

--- digi4k/lib/objects/inputparser.py
+++ digi4k/lib/objects/inputparser.py
@@ -7,12 +7,13 @@ class InputParser:
         self.front_end = 166 * (2 / 3)
         self.back_end = 166 * (1 / 3)
 
-    def try_hit_note(self, current_time: float, lanes: list[int], chart: Chart) -> bool:
+    def try_hit_note(self, current_time: float, lanes: list[int], chart: Chart) -> int:
         notes = chart.notes
+        hits = 0
         for lane in lanes:
             good_notes: list[ChartNote] = sorted([note for note in notes if note.lane == lane and note.hittable(current_time, (self.front_end, self.back_end))])
             if good_notes:
                 good_notes[0].hit = True
                 good_notes[0].hit_time = current_time
-                return True
-        return False
+                hits += 1
+        return hits
