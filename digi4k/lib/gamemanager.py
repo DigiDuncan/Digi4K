266544d1057ae4c7b676053f4ff2283e0f2b8e1b
DigiDuncan <digiduncan@gmail.com>
2021-10-01 04:58:43 -0400

    switch to test chart, add hit counter

--- digi4k/lib/gamemanager.py
+++ digi4k/lib/gamemanager.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+from importlib.resources import path
+
+from pygame.color import Color
+
 from digi4k.lib.objects.inputparser import InputParser
 from digi4k.lib.objects.keybinder import KeyBinder
 
@@ -12,20 +16,21 @@ from importlib.resources import files
 
 from nygame import music
 
-import digi4k.data.tutorial
+import digi4k.data.charts.test
 from digi4k.lib.draw_objects import EventViewer, Highway
 from digi4k.lib.inputmanager import InputManager
 from digi4k.lib.objects.note import Song
+from digi4k.lib import ptext
 
 
 class GameManager:
     def __init__(self, game: Game):
         self.game = game
-        self.song_file = j = json.loads(files(digi4k.data.tutorial).joinpath("tutorial-hard.json").read_text())
+        self.song_file = j = json.loads(files(digi4k.data.charts.test).joinpath("test.json").read_text())
         self.song = Song.from_json(j)
         self.highway_p1 = Highway(self.song.charts[0])
         self.highway_p2 = Highway(self.song.charts[1])
-        music.play(files(digi4k.data.tutorial).joinpath("Tutorial_Inst.ogg"))
+        music.play(files(digi4k.data.charts.test).joinpath("Inst.ogg"))
 
         self.input = InputManager()
         self.inputparser = InputParser(0, 0)
@@ -33,6 +38,11 @@ class GameManager:
         self.keybinder = KeyBinder()
 
         self.eventviewer = EventViewer(self.song.events)
+
+        self.hits = 0
+
+        with path(digi4k.data.fonts, "debug.ttf") as fp:
+            self.font = fp
 
     def update(self, events: list):
         now = music.elapsed
@@ -46,19 +56,20 @@ class GameManager:
             self.keybinder.right: 3
         }
 
-        lanes = [lanemap[k] for k in self.input.justPressed]
+        lanes = [lanemap[k] for k in self.input.justPressed if k in lanemap]
 
-        self.inputparser.try_hit_note(now, lanes, self.song.charts[0])
+        self.hits += self.inputparser.try_hit_note(now, lanes, self.song.charts[0])
+        ptext.draw(str(self.hits), midtop = self.game.surface.get_rect().midtop, fontname = self.font, fontsize = 100, color = Color(0x000000), surf = self.game.surface)
 
         # Draws
         self.highway_p1.update(now)
         self.highway_p2.update(now)
-        highway_p1_rect = self.highway_p1._image.get_rect()
+        highway_p1_rect = self.highway_p1.image.get_rect()
         highway_p1_rect.midright = self.game.surface.get_rect().midright
-        highway_p2_rect = self.highway_p2._image.get_rect()
+        highway_p2_rect = self.highway_p2.image.get_rect()
         highway_p2_rect.midleft = self.game.surface.get_rect().midleft
         eventviewer_rect = self.eventviewer.image.get_rect()
         eventviewer_rect.center = self.game.surface.get_rect().center
-        self.game.surface.blit(self.highway_p1._image, highway_p1_rect)
-        self.game.surface.blit(self.highway_p2._image, highway_p2_rect)
+        self.game.surface.blit(self.highway_p1.image, highway_p1_rect)
+        self.game.surface.blit(self.highway_p2.image, highway_p2_rect)
         self.game.surface.blit(self.eventviewer.image, eventviewer_rect)
